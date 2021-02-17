a51574f7c886ec9a08be22ef277b1f9241441600
lkjono02 <lkjoon02@naver.com>
2021-02-18 00:55:32 +0900

    2021-02-18-00:55

--- baekjoon/1011.py
+++ baekjoon/1011.py
@@ -2,7 +2,13 @@ import sys
 T = int(sys.stdin.readline().rstrip())
 for _ in range(T):
     x, y = map(int, sys.stdin.readline().rstrip().split())
-    k = [0, 1, 2]
-    cnt = 1
-    a = y-x-1
-    
+    dis = y - x
+    cnt = 0
+    move = 1
+    move_plus = 0
+    while move_plus < dis:
+        cnt += 1
+        move_plus += move
+        if cnt % 2 == 0:
+            move +=  1
+    print(cnt)
